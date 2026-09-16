e770002ebde53b1975ec5a56f288ea4ee88c20d4
timakibenedict619-hue <timakibenedict619@gmail.com>
2026-09-16 19:08:49 +0100

    Add service worker for caching files

---
+++ service-worker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = "msg-church-v1";
+
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
